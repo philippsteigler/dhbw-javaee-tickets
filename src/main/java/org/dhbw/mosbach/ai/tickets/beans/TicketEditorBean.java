0e735af16b233b57e512dbb1933bee54bb6c651b
squanchy11 <philipp.steigler@mailbox.org>
2019-06-09 15:03:00 +0200

    Change editor search to be based on ticketDAO SQL queries and not lambdas over local lists

--- src/main/java/org/dhbw/mosbach/ai/tickets/beans/TicketEditorBean.java
+++ src/main/java/org/dhbw/mosbach/ai/tickets/beans/TicketEditorBean.java
@@ -1,11 +1,11 @@
 package org.dhbw.mosbach.ai.tickets.beans;
 
+import com.google.common.collect.ImmutableList;
 import org.dhbw.mosbach.ai.tickets.database.EntryDAO;
 import org.dhbw.mosbach.ai.tickets.database.TicketDAO;
 import org.dhbw.mosbach.ai.tickets.model.Entry;
 import org.dhbw.mosbach.ai.tickets.model.Ticket;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
@@ -25,28 +25,19 @@ public class TicketEditorBean extends AbstractBean {
     private EntryDAO entryDAO;
 
     @Inject
-    private TicketBean ticketBean;
-
-    @Inject
     private SecurityBean securityBean;
 
     private static final String VIEW_DETAILS = "editor-ticket-details";
-    private String RETURN_ADDRESS = "";
+    private String REDIRECT = "";
 
-    private List<Ticket> tickets;
     private Ticket currentTicket;
     private List<Entry> currentEntries;
 
     private boolean rendered = false;
     private List<Ticket> searchResult;
     private String searchString = "";
-    private String entryContent = "";
 
-    @PostConstruct
-    public void init() {
-        this.tickets = ticketDAO.getAllFullyLoaded();
-        this.currentTicket = null;
-    }
+    private String entryContent = "";
 
     private void saveTicket(Ticket ticket) {
         ticketDAO.persistOrMerge(ticket);
@@ -57,17 +48,6 @@ public class TicketEditorBean extends AbstractBean {
         entryDAO.persistOrMerge(entry);
         addLocalizedFacesMessage(FacesMessage.SEVERITY_INFO, "entry.saveSuccess");
     }
-
-    /*
-    public void createTicket() {
-        tickets.add(new Ticket());
-    }
-
-    public void deleteTicket(Ticket ticket) {
-        ticketDAO.removeDetached(ticket);
-        addLocalizedFacesMessage(FacesMessage.SEVERITY_INFO, "ticket.deleteSuccess");
-    }
-     */
 
     // Search in Editors tickets
     private void checkRender(int size) {
@@ -90,23 +70,6 @@ public class TicketEditorBean extends AbstractBean {
         return searchResult;
     }
 
-    // Get tickets currently owned by the logged in editor
-    public void fetchMyTickets() {
-        searchResult = ticketBean.doSearch(tickets.stream().filter(ticket -> ticket.getEditorId() == securityBean.getUser().getId()).collect(Collectors.toList()), searchString);
-        checkRender(searchResult.size());
-    }
-
-    // Get all tickets
-    public void fetchAllTickets() {
-        searchResult = ticketBean.doSearch(tickets, searchString);
-        checkRender(searchResult.size());
-    }
-
-    // View ticket details
-    private void fetchTicketEntries() {
-        this.currentEntries = currentTicket.getEntries();
-    }
-
     public Ticket getCurrentTicket() {
         return currentTicket;
     }
@@ -115,12 +78,40 @@ public class TicketEditorBean extends AbstractBean {
         return currentEntries;
     }
 
-    public String viewTicketDetails(long id, String returnAddress) {
-        currentTicket = tickets.stream().filter(ticket -> ticket.getId() == id).collect(Collectors.toList()).get(0);
-        RETURN_ADDRESS = returnAddress;
-        fetchTicketEntries();
+    public String viewTicketDetails(Ticket ticket, String redirect) {
+        currentTicket = ticket;
+        currentEntries = currentTicket.getEntries();
+        REDIRECT = redirect;
 
         return VIEW_DETAILS;
+    }
+
+    public void fetchMyTickets() {
+        if (searchString.isEmpty()) {
+            searchResult = ImmutableList.copyOf(ticketDAO.getAllTicketsForID(securityBean.getUser().getId()));
+        } else {
+            searchResult = ImmutableList.copyOf(ticketDAO.getTicketsContainingSubjectForID(searchString, securityBean.getUser().getId()));
+        }
+
+        checkRender(searchResult.size());
+    }
+
+    public void fetchAllTickets() {
+        if (searchString.isEmpty()) {
+            searchResult = ticketDAO.getAll();
+        } else {
+            searchResult = ImmutableList.copyOf(ticketDAO.getTicketsContainingSubject(searchString));
+        }
+
+        checkRender(searchResult.size());
+    }
+
+    public void addEntryToTicket(String content) {
+        Entry newEntry = new Entry(securityBean.getUser().getId(), content, new Date());
+        currentTicket.addEntry(newEntry);
+        saveEntry(newEntry);
+        saveTicket(currentTicket);
+        entryContent = "";
     }
 
     public void setEntryContent(String entryContent) {
@@ -131,28 +122,22 @@ public class TicketEditorBean extends AbstractBean {
         return entryContent;
     }
 
-    // Edit ticket details
-    public void addEntryToTicket(String content) {
-        Entry newEntry = new Entry(securityBean.getUser().getId(), content, new Date());
-        currentTicket.addEntry(newEntry);
-        saveEntry(newEntry);
-        saveTicket(currentTicket);
-        entryContent = "";
-    }
-
     public String delegateTicket(long editorId){
         currentTicket.setEditorId(editorId);
         currentTicket.setStatusToInProcess();
         saveTicket(currentTicket);
 
-        return RETURN_ADDRESS;
+        currentTicket = null;
+        return REDIRECT;
     }
 
     public String releaseTicket() {
+        // TODO: Only release MY tickets
         currentTicket.setStatusToOpen();
         currentTicket.setEditorId(0);
         saveTicket(currentTicket);
 
-        return RETURN_ADDRESS;
+        currentTicket = null;
+        return REDIRECT;
     }
 }
--- src/main/java/org/dhbw/mosbach/ai/tickets/database/TicketDAO.java
+++ src/main/java/org/dhbw/mosbach/ai/tickets/database/TicketDAO.java
@@ -11,8 +11,38 @@ import java.util.List;
 public class TicketDAO extends BaseDAO<Ticket, Long>{
     private static final long serialVersionUID = 7107642207441127133L;
 
-    public TicketDAO()
-    {
+    public TicketDAO() {
         super();
+    }
+
+    public List<Ticket> getAllTicketsForID(long id) {
+        if (id >= 0) {
+            return em.createQuery("FROM Ticket t WHERE t.editorId = :editorId", Ticket.class)
+                    .setParameter("editorId", id)
+                    .getResultList();
+        }
+
+        return Collections.emptyList();
+    }
+
+    public List<Ticket> getTicketsContainingSubjectForID(String substring, long id) {
+        if ((substring != null) && (substring.length() >= 2) && (id >= 0)) {
+            return em.createQuery("FROM Ticket t WHERE UPPER(t.subject) LIKE :subject AND t.editorId = :editorId", Ticket.class)
+                    .setParameter("subject", "%" + substring.toUpperCase() + "%")
+                    .setParameter("editorId", id)
+                    .getResultList();
+        }
+
+        return Collections.emptyList();
+    }
+
+    public List<Ticket> getTicketsContainingSubject(String substring) {
+        if ((substring != null) && (substring.length() >= 2)) {
+            return em.createQuery("FROM Ticket t WHERE UPPER(t.subject) LIKE :subject", Ticket.class)
+                    .setParameter("subject", "%" + substring.toUpperCase() + "%")
+                    .getResultList();
+        }
+
+        return Collections.emptyList();
     }
 }
