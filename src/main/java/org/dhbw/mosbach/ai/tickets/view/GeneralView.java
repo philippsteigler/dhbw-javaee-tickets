413b95fc63a2ca84bf3f41ff65e243f87a154e9c
Jarno <wagnerjarno@gmail.com>
2019-06-10 19:05:17 +0200

    basic myprofile

--- src/main/java/org/dhbw/mosbach/ai/tickets/view/GeneralView.java
+++ src/main/java/org/dhbw/mosbach/ai/tickets/view/GeneralView.java
@@ -1,14 +1,42 @@
 package org.dhbw.mosbach.ai.tickets.view;
 
+import org.dhbw.mosbach.ai.tickets.beans.SecurityBean;
+import org.dhbw.mosbach.ai.tickets.beans.TicketEditorBean;
+import org.dhbw.mosbach.ai.tickets.model.Role;
+import org.dhbw.mosbach.ai.tickets.model.User;
+
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.text.DateFormat;
-import java.util.Date;
+import java.util.*;
 
 @Named
 @SessionScoped
 public class GeneralView implements Serializable {
+
+    @Inject
+    private SecurityBean securityBean;
+
+    private String name = "";
+    private String userName = "";
+    private String email = "";
+    private String company = "";
+    private Set<Role> roles = new HashSet<>();
+
+
+
+    public void getUserToDisplay(){
+        User user = securityBean.getUser();
+
+        name = user.getName();
+        userName = user.getLoginID();
+        email = user.getEmail();
+        company = user.getCompany();
+        roles = user.getRoles();
+    }
+
 
     public String formatDate(Date date){
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.MEDIUM);
@@ -16,4 +44,46 @@ public class GeneralView implements Serializable {
         return dateFormat.format(date);
     }
 
+    //Getter and Setter
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
