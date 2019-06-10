d49eda1a2691575ed3b15bab715a359394d7acbb
Jarno <wagnerjarno@gmail.com>
2019-06-10 14:27:40 +0200

    creating new user

--- src/main/java/org/dhbw/mosbach/ai/tickets/beans/UserBean.java
+++ src/main/java/org/dhbw/mosbach/ai/tickets/beans/UserBean.java
@@ -3,6 +3,7 @@ package org.dhbw.mosbach.ai.tickets.beans;
 import com.google.common.collect.ImmutableList;
 import org.dhbw.mosbach.ai.tickets.database.UserDAO;
 import org.dhbw.mosbach.ai.tickets.model.Role;
+import org.dhbw.mosbach.ai.tickets.model.Roles;
 import org.dhbw.mosbach.ai.tickets.model.User;
 
 import javax.enterprise.context.SessionScoped;
@@ -11,6 +12,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Named
@@ -29,16 +31,36 @@ public class UserBean extends AbstractBean {
     private List<User> searchResult;
     private String searchString = "";
 
+    private String login_id;
+    private String name;
+    private String companyName;
+    private String email;
+    private String password;
+    private String role;
+
+    private List<Role> roles;
+
     private static final String VIEW_DETAILS = "admin-user-details";
     private static final String VIEW_USERS = "admin-all-users";
 
-    private String newUser(String login, String userName, String companyName, String email, String password, Role... userRoles) {
-        final User user = new User(login, userName, companyName, email);
-        user.getRoles().addAll(Arrays.asList(userRoles));
+    public String newUser(String login_id, String name, String companyName, String email, String password, String role) {
+        final User user = new User(login_id, name, companyName, email);
+        user.getRoles().add(parseRoles(role));
         userDAO.changePassword(user, password);
         saveUser(user);
 
         return VIEW_USERS;
+    }
+
+    private Role parseRoles(String role){
+
+        for (Role roleFromDatabase : roles) {
+            if (role.equals(roleFromDatabase.getName())) {
+                return roleFromDatabase;
+            }
+        }
+
+        return null;
     }
 
     private void saveUser(User user) {
@@ -81,6 +103,10 @@ public class UserBean extends AbstractBean {
         }
     }
 
+    public void fetchAllRoles() {
+        roles = userDAO.getRoles();
+    }
+
     public List<User> getEditors() {
         return userDAO.getAll().stream().filter(user -> user.getRoles().stream().allMatch(role -> role.getName().equals("editor")
                 && user.getId() != securityBean.getUser().getId())).collect(Collectors.toList());
@@ -96,5 +122,57 @@ public class UserBean extends AbstractBean {
 
     public String getUserCompany(long id) {
         return userDAO.getAll().stream().filter(user -> user.getId() == id).collect(Collectors.toList()).get(0).getCompany();
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public String getLogin_id() {
+        return login_id;
+    }
+
+    public void setLogin_id(String login_id) {
+        this.login_id = login_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
