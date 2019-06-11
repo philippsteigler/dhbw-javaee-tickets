f5b32715505186e1b64dfa4e63f528022acf8164
HazeHawk <roman.wuertemberger@gmail.com>
2019-06-11 09:36:17 +0200

    cleanup

--- src/main/java/org/dhbw/mosbach/ai/tickets/beans/FilterBean.java
+++ src/main/java/org/dhbw/mosbach/ai/tickets/beans/FilterBean.java
@@ -1,13 +1,8 @@
 package org.dhbw.mosbach.ai.tickets.beans;
 
-import org.dhbw.mosbach.ai.tickets.model.Ticket;
-
 import javax.enterprise.context.SessionScoped;
-import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.ArrayList;
-import java.util.List;
 
 @Named
 @SessionScoped
